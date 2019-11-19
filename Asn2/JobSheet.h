8ca8665d1eb7fbcf8865b2703524f7ae1c4e6c1e
shalinhudda97 <shalin.hudda97@hotmail.com>
2019-11-18 21:44:49 -0800

    Fixing bug in previous commit

--- Asn2/JobSheet.h
+++ Asn2/JobSheet.h
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "LinkedList.h"
 
-<<<<<<< HEAD
 struct Data {
 	int PartNumber;
 	int Quantity;
@@ -13,9 +12,5 @@ struct Data {
 class JobSheet {
 	LinkedList<Data> JobSheetList;
 public:
-	
-=======
-class JobSheet {
 
->>>>>>> bab04a23dcffe7e84888b56225798c33969f70ae
 };
