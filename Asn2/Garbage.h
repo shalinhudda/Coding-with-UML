f8b203ced1979ceee69a5d5f1bdd9a2b7af50cb2
shalinhudda97 <shalin.hudda97@hotmail.com>
2019-11-19 14:43:23 -0800

    Added garbage and recycling

--- Asn2/Garbage.h
+++ Asn2/Garbage.h
@@ -1,1 +1,12 @@
 #pragma once
+
+struct OilFilter;
+struct AirFilter;
+
+class Garbage {
+public:
+
+	void Dispose(OilFilter* OldOilFilter);
+	void Dispose(AirFilter* OldAirFilter);
+	~Garbage() {};
+};
--- Asn2/Recycling.h
+++ Asn2/Recycling.h
@@ -1,1 +1,12 @@
 #pragma once
+
+struct Oil;
+struct Tires;
+
+class Recycling {
+public:
+
+	void Recycle(Oil* OldOil);
+	void Recycle(Tires* OldTires);
+	~Recycling() {};
+};
